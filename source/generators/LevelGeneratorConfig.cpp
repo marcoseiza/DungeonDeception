7377a459550bf8a353c7131374b8a059ded65721
Marcos Eizayaga <me437@cornell.edu>
2022-04-15 11:11:57 -0400

    change level gen config

--- source/generators/LevelGeneratorConfig.cpp
+++ source/generators/LevelGeneratorConfig.cpp
@@ -4,14 +4,14 @@ namespace level_gen {
 
 LevelGeneratorConfig::LevelGeneratorConfig()
     : _inner_circle_frac(0.4f),
-      _middle_circle_frac(0.7f),
+      _middle_circle_frac(0.75f),
       _hallway_radius(1),
       _num_rooms(100),
-      _separation_between_layers(15),
+      _separation_between_layers(10),
       _max_hallway_length(40),
       _add_edges_back_prob(0.2f),
       _max_num_of_edges(4) {
-  setMapRadius(120);
+  setMapRadius(90);
   setNumTerminalRooms(7);
 }
 
