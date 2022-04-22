bf23a11932c2d1257544428c44df8bfc35287cad
Will Spencer <will.e.spencer@gmail.com>
2022-04-22 12:21:42 -0400

    Create a PeerLobbyScene with common code

--- source/scenes/PeerLobbyScene.h
+++ source/scenes/PeerLobbyScene.h
@@ -1,16 +1,27 @@
-#ifndef SCENES_CLIENT_LOBBY_SCENE_H_
-#define SCENES_CLIENT_LOBBY_SCENE_H_
+#ifndef SCENES_PEER_LOBBY_SCENE_H_
+#define SCENES_PEER_LOBBY_SCENE_H_
 #include <cugl/cugl.h>
 
 #include <vector>
-
-#include "PeerLobbyScene.h"
 
 /**
  * This class provides the interface for clients to wait for a game to start.
  *
  */
-class ClientLobbyScene : public PeerLobbyScene {
+class PeerLobbyScene : public cugl::Scene2 {
+ public:
+  /**
+   * The configuration status.
+   */
+  enum Status {
+    /** Client or Host is waiting to start game */
+    WAIT,
+    /** Time to start the game */
+    START,
+    /** Game was aborted; back to main menu */
+    ABORT
+  };
+
  protected:
   /** The asset manager for this scene. */
   std::shared_ptr<cugl::AssetManager> _assets;
@@ -32,7 +43,7 @@ class ClientLobbyScene : public PeerLobbyScene {
   cugl::NetworkDeserializer _deserializer;
 
   /** The current status */
-  PeerLobbyScene::Status _status;
+  Status _status;
 
   /** The map seed. */
   Uint64 _seed;
@@ -44,19 +55,19 @@ class ClientLobbyScene : public PeerLobbyScene {
 #pragma mark -
 #pragma mark Constructors
   /**
-   * Creates a new client scene with the default values.
+   * Creates a new scene with the default values.
    */
-  ClientLobbyScene() : PeerLobbyScene() {}
+  PeerLobbyScene() : cugl::Scene2() {}
 
   /**
    * Disposes of all (non-static) resources allocated to this mode.
    */
-  ~ClientLobbyScene() { dispose(); }
+  ~PeerLobbyScene() { dispose(); }
 
   /**
    * Disposes of all (non-static) resources allocated to this mode.
    */
-  virtual void dispose() override;
+  virtual void dispose() override{};
 
   /**
    * Initializes the controller contents, and starts the game
@@ -65,29 +76,9 @@ class ClientLobbyScene : public PeerLobbyScene {
    *
    * @return true if the controller is initialized properly, false otherwise.
    */
-  virtual bool init(const std::shared_ptr<cugl::AssetManager>& assets) override;
-
-  /**
-   * Sets whether the scene is currently active
-   *
-   * @param value whether the scene is currently active
-   */
-  virtual void setActive(
-      bool value, std::shared_ptr<cugl::NetworkConnection> network) override;
-
-  /**
-   * The method called to update the scene.
-   *
-   * @param timestep  The amount of time (in seconds) since the last frame
-   */
-  virtual void update(float timestep) override;
-
-  /**
-   * Sets the game id from the network to the lobby
-   *
-   * @param id the game id
-   */
-  virtual void setGameId(string id) { _gameid->setText(id); };
+  virtual bool init(const std::shared_ptr<cugl::AssetManager>& assets) {
+    return false;
+  };
 
   /**
    * Returns the scene status. Any value other than WAIT will transition to
@@ -97,13 +88,55 @@ class ClientLobbyScene : public PeerLobbyScene {
    */
   Status getStatus() const { return _status; }
 
+  /**
+   * Returns if the scene represents a host or betrayer
+   * @return The role of the player, true if betrayer, false otherwise.
+   */
+  bool isBetrayer() { return _is_betrayer; }
+
+  /**
+   * Sets whether the scene is currently active
+   *
+   * @param value whether the scene is currently active
+   */
+  virtual void setActive(bool value,
+                         std::shared_ptr<cugl::NetworkConnection> network){};
+
+  /**
+   * Returns the seed to be broadcast to all clients.
+   * @return The seed for the map
+   */
+  Uint64 getSeed() { return _seed; }
+
+  /**
+   * Returns the player name to be used in game.
+   * @return The player name
+   */
+  std::string getPlayerName() { return _name->getText(); }
+
+  /**
+   * The method called to update the scene.
+   *
+   * @param timestep  The amount of time (in seconds) since the last frame
+   */
+  virtual void update(float timestep) override{};
+
+  /**
+   * Disconnects this scene from the network controller.
+   *
+   * Technically, this method does not actually disconnect the network
+   * controller. Since the network controller is a smart pointer, it is only
+   * fully disconnected when ALL scenes have been disconnected.
+   */
+  void disconnect() { _network = nullptr; }
+
  private:
   /**
    * Checks that the network connection is still active.
    *
    * @return true if the network connection is still active.
    */
-  virtual bool checkConnection() override;
+  virtual bool checkConnection() { return false; };
 
   /**
    * Processes data (byte vectors) sent over the network.
@@ -113,7 +146,7 @@ class ClientLobbyScene : public PeerLobbyScene {
    *
    * @param data  The data received
    */
-  virtual void processData(const std::vector<uint8_t>& data) override;
+  virtual void processData(const std::vector<uint8_t>& data){};
 };
 
-#endif /* SCENES_CLIENT_LOBBY_SCENE_H_ */
+#endif /* SCENES_PEER_LOBBY_SCENE_H_ */
